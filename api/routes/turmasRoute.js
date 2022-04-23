9dc402b5f502a9cf659600a3d88aa20415cc32cd
EmanueleRangel <emanuele.rangel52@gmail.com>
2022-04-23 11:34:19 -0300

    services layer added

--- api/routes/turmasRoute.js
+++ api/routes/turmasRoute.js
@@ -6,8 +6,8 @@ router
  .get('/turmas', TurmaController.pegaTodasAsTurmas)
  .get('/turmas/:id', TurmaController.pegaUmaTurma)
  .post('/turmas', TurmaController.criaTurma)
+ .post('/turmas/:id/restaura', TurmaController.restauraTurma)
  .put('/turmas/:id', TurmaController.atualizaTurma)
  .delete('/turmas/:id', TurmaController.apagaTurma)
- .post('/turmas/:id/restaura', TurmaController.restauraTurma)
- 
+
 module.exports = router
